b23953cb1a2a7706d3d6b018e53e60cd248f5da9
Waelpu <waelkaraa55@gmail.com>
2022-12-01 23:59:32 +0100

    checkpoint jsx

---
+++ src/profile/FullName.js
@@ -0,0 +1,9 @@
+function FullName(){
+    return(
+       <div className="name"> 
+        <h3 >Full Name : Wael Karaa</h3>
+        <h3>E-mail : waelkaraa00@gmail.com</h3>
+        </div>
+    );
+}
+export default FullName;
